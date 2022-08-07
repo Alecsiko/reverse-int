ca9bf3afc16bd428c46d791e587f70f93d00cf4e
Aleksey <104904174+Alecsiko@users.noreply.github.com>
2022-08-07 12:56:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let a = Math.abs(n);
+    let b = a.toString();
+    let c = b.split('');
+    let d = c.reverse();
+    let i = parseInt(d.join(''))
+    
+    return(i);
+    
 }
