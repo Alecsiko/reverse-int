312a9d1b3d231806a0c787848fbe53963eecd4ce
Aleksey <104904174+Alecsiko@users.noreply.github.com>
2023-01-06 12:40:55 +0300

    fix: negativ number

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 module.exports = function reverse (n) {
-    let a = Math.abs(n);
-    let b = a.toString();
-    let c = b.split('');
-    let d = c.reverse();
-    let i = parseInt(d.join(''))
-    
-    return(i);
-//     return Number(String(n).split('').reverse().join('')) second decision
+//     let a = Math.abs(n);
+//     let b = a.toString();
+//     let c = b.split('');
+//     let d = c.reverse();
+//     let i = parseInt(d.join(''))
+//     return(i);
+  return parseInt(String(a).split('').reverse().join(''));  //second decision
 }
